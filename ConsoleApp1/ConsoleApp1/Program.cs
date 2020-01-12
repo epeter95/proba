a3cefe6a17dc287926f736d985e51ca919d53c6d
Péter Erdősi <peter.erdosi2@gmail.com>
2020-01-12 17:42:16 +0100

    1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,12 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello ghjkghjkhjWorld!");
+            for (int i = 0; i < 1; i++)
+            {
+
+            }
+
+            
         }
     }
 }
